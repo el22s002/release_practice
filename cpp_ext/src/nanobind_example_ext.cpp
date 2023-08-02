60a5fe0304af3bae042538970dc389cf439af7d3
el22s002 <141048556+el22s002@users.noreply.github.com>
2023-08-02 15:28:11 +0900

    Update nanobind_example_ext.cpp

--- cpp_ext/src/nanobind_example_ext.cpp
+++ cpp_ext/src/nanobind_example_ext.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <nanobind/nanobind.h>
-#include <opencv2/core.hpp>
+// #include <opencv2/core.hpp>
 #include <omp.h>
 
 namespace nb = nanobind;
@@ -9,8 +9,8 @@ using namespace nb::literals;
 
 NB_MODULE(nanobind_example_ext, m) {
     m.def("add", [](int a, int b) { return a + b; }, "a"_a, "b"_a);
-    m.def("print_vec", []() { 
-        cv::Vec3f vec;
-        std::cout << vec << std::endl;
-    });
+    // m.def("print_vec", []() { 
+    //     cv::Vec3f vec;
+    //     std::cout << vec << std::endl;
+    // });
 }
